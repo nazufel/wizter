992cd602b12b048989eeaa24b110831906689f86
nazufel <38038720+nazufel@users.noreply.github.com>
2021-06-08 09:45:55 -0400

    i think it worked

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	pb "github.com/nazufel/telepresence-demo/wizard"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -24,16 +25,42 @@ type server struct {
 	pb.WizardServiceServer
 }
 
-type mongoStorage struct {
-	db *mongo.Database
-	c  *mongo.Client
-}
-
 func main() {
 	log.Println("starting wizards server")
-	log.Println("trying to connect to database")
 
-	ms := new(mongoStorage)
+	log.Println("dropping the wizards collection and seeding database")
+	err := seedData()
+	if err != nil {
+		log.Fatalf("failed to seed the DB: %v", err)
+	}
+	log.Printf("finished seeding the database")
+
+	lis, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("could not listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterWizardServiceServer(grpcServer, &server{})
+
+	log.Printf("running grpc server on port: %v", grpcPort)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to start the grpc server: %s", err)
+	}
+}
+
+type WizardRecord struct {
+	Name       string `bson:"name"`
+	House      string `bson:"house"`
+	DeathEater bool   `bson:"death_eater"`
+}
+
+func (s *server) List(e *pb.EmptyRequest, srv pb.WizardService_ListServer) error {
+
+	log.Println("sending list of wizards to client")
+
 	client, err := mongo.NewClient(options.Client().ApplyURI(dbConnection))
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
@@ -53,70 +80,64 @@ func main() {
 		log.Fatalf("error pinging DB: %v", err)
 	}
 
-	ms.db = client.Database("wizards")
-	ms.c = client
-	log.Printf("connected to the database")
+	db := client.Database("wizards")
 
-	log.Println("dropping the wizards collection")
-	err = ms.seedData()
+	cursor, err := db.Collection("Wizards").Find(ctx, bson.M{})
 	if err != nil {
-		log.Fatalf("failed to seed the DB: %v", err)
+		log.Printf("failed to get wizards: %v", err)
 	}
+	defer cursor.Close(context.Background())
 
-	lis, err := net.Listen("tcp", grpcPort)
-	if err != nil {
-		log.Fatalf("could not listen: %v", err)
-	}
+	for cursor.Next(context.Background()) {
+		var wizard pb.Wizard
 
-	grpcServer := grpc.NewServer()
-
-	pb.RegisterWizardServiceServer(grpcServer, &server{})
-
-	log.Printf("running grpc server on port: %v", grpcPort)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to start the grpc server: %s", err)
-	}
-}
-
-func (s *server) List(e *pb.EmptyRequest, srv pb.WizardService_ListServer) error {
-
-	var err error
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbConnection))
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
+		err := cursor.Decode(&wizard)
+		if err != nil {
+			log.Printf("unable to decode wizard cursor into struct: %v", err)
 		}
-	}()
-	collection := client.Database("wizards").Collection("wizards")
 
-	log.Printf("collection: %v", collection)
-	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+		err = srv.Send(&wizard)
+		if err != nil {
+			log.Printf("send error: %v", err)
+		}
+		log.Printf("sent wizard to client: %v", wizard.GetName())
+	}
 
-	err = client.Ping(ctx, readpref.Primary())
+	err = cursor.Err()
 	if err != nil {
-		log.Fatalf("error pinging DB: %v", err)
+		log.Printf("error with the client cursor: %v", err)
 	}
 
 	return nil
 
 }
 
-type WizardRecord struct {
-	Name       string `bson:"name"`
-	House      string `bson:"house"`
-	DeathEater bool   `bson:"death_eater"`
-}
-
 // seedData drops the wizards collection and seeds it with fresh data to the demo
-func (m *mongoStorage) seedData() error {
+func seedData() error {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbConnection))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	defer func() {
+		err = client.Disconnect(ctx)
+		if err != nil {
+			log.Fatalf("failed to disconnect client:  %v", err)
+		}
+	}()
+
+	// test connection
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalf("error pinging DB: %v", err)
+	}
+
+	db := client.Database("wizards")
 
 	// // drop the collection in order to see fresh data for a new run
-	err := m.db.Collection("wizards").Drop(context.Background())
+	err = db.Collection("wizards").Drop(context.Background())
 	if err != nil {
 		log.Fatal("unable drop the wizard collection")
 	}
@@ -145,12 +166,10 @@ func (m *mongoStorage) seedData() error {
 			DeathEater: false,
 		},
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
+	log.Printf("connected to the database")
 
 	for i := range wizards {
-		_, err = m.db.Collection("wizards").InsertOne(ctx, wizards[i])
+		_, err = db.Collection("wizards").InsertOne(ctx, wizards[i])
 		if err != nil {
 			log.Fatalf("failed to insert document: %v", err)
 		}
@@ -158,7 +177,6 @@ func (m *mongoStorage) seedData() error {
 		i++
 	}
 
-	log.Printf("finished seeding the database")
 	log.Printf("inserted %v documents", len(wizards))
 
 	return err
