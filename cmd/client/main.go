b8adc5371b7da925b2d20597f868493d520127f5
nazufel <38038720+nazufel@users.noreply.github.com>
2021-06-11 12:47:36 -0400

    client works locally pointed at the remote sever

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -57,7 +57,7 @@ func main() {
 
 func clientCall() {
 
-	address := "localhost" + ":" + os.Getenv("WIZARDS_SERVER_GRPC_PORT")
+	address := os.Getenv("WIZARDS_SERVER_SERVICE_HOST") + ":" + os.Getenv("WIZARDS_SERVER_GRPC_PORT")
 	log.Printf("connection address: %s", address)
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
