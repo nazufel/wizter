c8652280f6821d8f999e567c9032267e82be8309
nazufel <38038720+nazufel@users.noreply.github.com>
2021-09-23 14:14:47 -0400

    not working still

--- cmd/server/mongo.go
+++ cmd/server/mongo.go
@@ -7,12 +7,12 @@ import (
 	"time"
 
 	pb "github.com/nazufel/wizter/wizard"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-// storage struct holds information about connecting to storage
 type storage struct {
 	c  *mongo.Client
 	db *mongo.Database
@@ -23,12 +23,12 @@ func dbConnect() (*storage, error) {
 
 	log.Printf("connecting to database host: %v", os.Getenv("MONGO_HOST"))
 
-	s := new(storage)
-	
 	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_HOST")))
 	if err != nil {
 		log.Printf("new client error: %v", err)
 	}
+
+	st := new(storage)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
@@ -41,10 +41,10 @@ func dbConnect() (*storage, error) {
 		log.Fatalf("error pinging DB: %v", err)
 	}
 
-	s.c = client
-	s.db = client.Database("wizards")
+	st.c = client
+	st.db = client.Database("wizards")
 
-	return s, nil
+	return st, nil
 }
 
 // seedData drops the wizards collection and seeds it with fresh data to the demo
@@ -121,4 +121,67 @@ func (s *storage) seedData() error {
 	log.Printf("inserted %v documents", len(wizards))
 
 	return err
+}
+
+func (st *storage) clientResponse(srv pb.WizardService_ListServer) error {
+
+	log.Printf("storage: %v", st.db)
+
+	// set find options behavior
+	findOptions := options.Find()
+	// findOptions.SetLimit(25)
+
+	// filter by company, this is the default behavior
+	filter := bson.D{{}}
+
+	cursor, err := st.db.Collection("wizards").Find(context.Background(), filter, findOptions)
+	if err != nil {
+		log.Printf("failed to get wizards: %v", err)
+	}
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var wizard pb.Wizard
+
+		err := cursor.Decode(&wizard)
+		if err != nil {
+			log.Printf("unable to decode wizard cursor into struct: %v", err)
+		}
+
+		// Commenting out. Uncomment when making the server change in the demo
+		// switch house := wizard.House; house {
+		// case "Gryffindor":
+		// 	log.Printf("%v - sending wizard to client: %v", emoji.Eagle, wizard.GetName())
+		// case "Ravenclaw":
+		// 	log.Printf("%v - sending wizard to client: %v", emoji.Bird, wizard.GetName())
+		// case "Hufflepuff":
+		// 	log.Printf("%v - sending wizard to client: %v", emoji.Badger, wizard.GetName())
+		// case "Slytherin":
+		// 	log.Printf("%v - sending wizard to client: %v", emoji.Snake, wizard.GetName())
+		// }
+
+		// comment this log statement as part of the server demo
+		log.Printf("sending wizard to client: %v", wizard.GetName())
+
+		err = srv.Send(&wizard)
+		if err != nil {
+			log.Printf("send error: %v", err)
+		}
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		log.Printf("error with the client cursor: %v", err)
+	}
+
+	// uncomment before building the docker image with Bazel
+	// log.Println("")
+	// log.Println("built with Bazel")
+	// log.Println("")
+
+	log.Println("# -------------------------------------- #")
+	log.Println("done sending list of wizards to client")
+	log.Println("# -------------------------------------- #")
+
+	return nil
 }
